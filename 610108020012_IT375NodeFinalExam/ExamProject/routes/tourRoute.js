f5c9ae025df1c90d9e9372e0d739d01e260021e8
Pansapol <610108020012@dpu.ac.th>
2020-11-06 15:05:30 +0700

    Exam

---
+++ 610108020012_IT375NodeFinalExam/ExamProject/routes/tourRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const tourController = require('./../controllers/tourController');
+
+router
+.route('/')
+.get(tourController.getAllTours)
+.post(tourController.createNewTour);
+
+//router.get('/',restaurantController.getAllRestaurants);
+
+router
+.route('/:id')
+.get(tourController.getOneTour)
+.put(tourController.updateTour)
+.delete(tourController.deleteTour);
+
+module.exports = router;
